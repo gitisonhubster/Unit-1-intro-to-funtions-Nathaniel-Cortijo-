2214ee18bdf846eccbd946750e37168a567c181e
gitisonhubster <Nathanielc140@nycstudents.net>
2026-09-17 10:32:23 -0400

    Update test.py

--- function test/test.py
+++ function test/test.py
@@ -16,19 +16,33 @@ t.shape('turtle')
 def add(x,y):
     return x + y
 
-
+sidelength = 100
+rotate = 90
 def square(x,y):
     for i in range(4):
         t.forward(x)
         t.left(y)
-square(100,90)
+""" square(100,90) """
 
 def triangle(x,y):
     for i in range(6):
         t.forward(x)
         t.left(y)
-triangle(100,120)
+""" triangle(100,120) """
 
+def square_sircle (x,y):
+    for i in range(60):
+       square(x,y)
+       t.right(5)
+""" square_sircle(100,90) """
 
+def doubleSquares(iRange):
+    length = 25
+    for i in range(iRange):
+        square(length, 90)
+        length = length * 2
+doubleSquares(5)
+
+n = add(n,1)
 
 turtle.done
